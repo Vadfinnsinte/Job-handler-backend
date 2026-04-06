8cd91f5280c98de9b84fc4e31699a3df4a273aae
natta1995 <natalie.hallerdal@yahoo.se>
2026-04-06 10:10:52 +0200

    Remove DTO Post and User. ( Comments left in projekt)

--- Dtos/User/UserUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobHandlerAPI.DTOs.User
-{
-    public class UserUpdateDto
-    {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public bool EmploymentStatus { get; set; }
-    }
-}
